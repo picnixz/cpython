bb3b4823794a1285cb06c02c42d2af2da13a284f
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-01-19 15:50:07 +0100

    Add `rlcompleter.Filter` tests

--- Lib/test/test_rlcompleter.py
+++ Lib/test/test_rlcompleter.py
@@ -1,7 +1,9 @@
 import unittest
 from unittest.mock import patch
 import builtins
+import itertools
 import rlcompleter
+from types import MappingProxyType
 from test.support import MISSING_C_DOCSTRINGS
 
 class CompleteMe:
@@ -167,6 +169,288 @@ class TestRlcompleter(unittest.TestCase):
         self.assertIsNone(completer.complete('memoryview', 1))
         self.assertEqual(completer.complete('Ellipsis', 0), 'Ellipsis()')
         self.assertIsNone(completer.complete('Ellipsis', 1))
+
+
+class TestFilterInterface(unittest.TestCase):
+
+    def test_default_priority(self):
+        f = rlcompleter.Filter()
+        self.assertEqual(f.priority, 500)
+
+    def test_default_namespace_filter(self):
+        f = rlcompleter.NamespaceFilter()
+        self.assertTrue(f.filter({}, "", None, ""))
+
+    def test_default_attribute_filter(self):
+        f = rlcompleter.AttributeFilter()
+        self.assertTrue(f.filter(object(), "", None, ""))
+
+
+@unittest.mock.patch('rlcompleter._readline_available', False)
+class BaseTestFilter:
+
+    filter_class: type[rlcompleter.Filter] = None
+
+    def get_completer(self, namespace=MappingProxyType({})):
+        return rlcompleter.Completer(namespace)
+
+    def new_filter(self, priority=None):
+        return self.filter_class(priority)
+
+    def add_filter(self, c, f):
+        raise NotImplementedError
+
+    def remove_filter(self, c, f):
+        raise NotImplementedError
+
+    def assert_filters_equal(self, c, fs):
+        raise NotImplementedError
+
+    def test_add_filter(self):
+        c = self.get_completer()
+        f = self.new_filter()
+        self.assertTrue(self.add_filter(c, f))
+        self.assert_filters_equal(c, [f])
+
+    def test_add_filters(self):
+        c = self.get_completer()
+
+        f1 = self.new_filter()
+        r1 = self.add_filter(c, f1)
+        self.assertTrue(r1, "first filter is already present")
+
+        f2 = self.new_filter()
+        r2 = self.add_filter(c, f2)
+        self.assertTrue(r2, "second filter is already present")
+
+        self.assert_filters_equal(c, [f1, f2])
+
+    def test_add_filters_with_priority(self):
+        c = self.get_completer()
+        self.add_filter(c, f0 := self.new_filter())
+        self.add_filter(c, f1 := self.new_filter(priority=20))
+        self.add_filter(c, f2 := self.new_filter(priority=10))
+        self.add_filter(c, f3 := self.new_filter(priority=10))
+        self.add_filter(c, f4 := self.new_filter())
+        self.assert_filters_equal(c, [f2, f3, f1, f0, f4])
+
+    def test_add_filters_no_duplicate(self):
+        c = self.get_completer()
+        f = self.new_filter()
+
+        r1 = self.add_filter(c, f)
+        self.assertTrue(r1, "first filter is missing")
+
+        r2 = self.add_filter(c, f)
+        self.assertFalse(r2, "not a duplicated filter")
+
+        self.assert_filters_equal(c, [f])
+
+    def test_remove_filter(self):
+        c = self.get_completer()
+        f = self.new_filter()
+        self.assertTrue(self.add_filter(c, f))
+        self.assertTrue(self.remove_filter(c, f))
+        self.assert_filters_equal(c, [])
+
+    def test_remove_missing_filter(self):
+        c = self.get_completer()
+        f = self.new_filter()
+        self.assertFalse(self.remove_filter(c, f))
+        self.assert_filters_equal(c, [])
+
+    def test_default_filter(self):
+        raise NotImplementedError
+
+    def test_simple_filter(self):
+        raise NotImplementedError
+
+    def test_chain_filters(self):
+        raise NotImplementedError
+
+
+class TestNamespaceFilter(BaseTestFilter, unittest.TestCase):
+
+    filter_class = rlcompleter.NamespaceFilter
+
+    def add_filter(self, c, f):
+        return c.add_namespace_filter(f)
+
+    def remove_filter(self, c, f):
+        return c.remove_namespace_filter(f)
+
+    def assert_filters_equal(self, c, fs):
+        self.assertListEqual(c.namespace_filters, fs)
+
+    def test_default_filter(self):
+        c = self.get_completer(dict.fromkeys(['my_foo', 'their_foo']))
+        self.add_filter(c, self.new_filter())
+        self.assertEqual(c.complete('my_', 0), 'my_foo')
+
+    def test_simple_filter(self):
+        class SimpleFilter(self.filter_class):
+            def filter(self, namespace, key, value, text, /, **options):
+                return not key.startswith('_')
+
+        c = self.get_completer(dict.fromkeys(['my_foo', '_sunder']))
+        self.add_filter(c, SimpleFilter())
+        self.assertEqual(c.complete('my_', 0), 'my_foo')
+        self.assertEqual(c.complete('_sunder', 0), None)
+
+    def test_chain_filters(self):
+
+        class SkipIf(self.filter_class):
+            def __init__(self, prefix):
+                super().__init__()
+                self.prefix = prefix
+
+            def filter(self, namespace, key, value, text, /, **options):
+                return not key.startswith(self.prefix)
+
+        c = self.get_completer(dict.fromkeys(['my1_skip', 'my2_skip', 'my3']))
+        self.add_filter(c, SkipIf('my1_skip'))
+        self.add_filter(c, SkipIf('my2_skip'))
+
+        matches = c.get_matches('my')
+        self.assertNotIn('my1_skip', matches)
+        self.assertNotIn('my2_skip', matches)
+        self.assertIn('my3', matches)
+
+
+class Object:
+    pick0 = ...
+    skip0 = ...
+    _sunder0 = ...
+    __dunder0 = ...
+    __ddunder0__ = ...
+
+    def __init__(self):
+        self.pick1 = ...
+        self.skip1 = ...
+        self._sunder1 = ...
+        self.__dunder1 = ...
+        self.__ddunder1__ = ...
+
+    def pick2(self): ...
+    def skip2(self): ...
+    def _sunder2(self): ...
+    def __dunder2(self): ...
+    def __ddunder2__(self): ...
+
+    @property
+    def pick3(self): ...
+    @property
+    def skip3(self): ...
+    @property
+    def _sunder3(self): ...
+    @property
+    def __dunder3(self): ...
+    @property
+    def __ddunder3__(self): ...
+
+    @classmethod
+    def pick4(cls): ...
+    @classmethod
+    def skip4(cls): ...
+    @classmethod
+    def _sunder4(cls): ...
+    @classmethod
+    def __dunder4(cls): ...
+    @classmethod
+    def __ddunder4__(cls): ...
+
+    @staticmethod
+    def pick5(): ...
+    @staticmethod
+    def skip5(): ...
+    @staticmethod
+    def _sunder5(): ...
+    @staticmethod
+    def __dunder5(): ...
+    @staticmethod
+    def __ddunder5__(): ...
+
+
+class TestAttributeFilter(BaseTestFilter, unittest.TestCase):
+
+    filter_class = rlcompleter.AttributeFilter
+
+    @classmethod
+    def setUpClass(cls):
+        cls.objname = 'my_object'  # should not be a keyword or a built-in
+        cls.prefixes = ('pick', 'skip', '_sunder',
+                        '_Object__dunder', '__ddunder')
+
+        class SkipIfContains(cls.filter_class):
+            def __init__(self, prefix):
+                super().__init__()
+                self.prefix = prefix
+
+            def filter(self, instance, name, value, text, /, **options):
+                return self.prefix not in name
+
+        cls.SkipIfContains = SkipIfContains
+
+    def add_filter(self, c, f):
+        return c.add_attribute_filter(f)
+
+    def remove_filter(self, c, f):
+        return c.remove_attribute_filter(f)
+
+    def assert_filters_equal(self, c, fs):
+        self.assertListEqual(c.attribute_filters, fs)
+
+    def make_matches(self, objname, prefix):
+        assert prefix in self.prefixes
+        suffix = '__' if prefix == '__ddunder' else ''
+
+        return [
+            f"{objname}.{prefix}0{suffix}",   # class attribute
+            f"{objname}.{prefix}1{suffix}",   # attribute
+            f"{objname}.{prefix}2{suffix}()", # method
+            f"{objname}.{prefix}3{suffix}",   # property
+            f"{objname}.{prefix}4{suffix}()", # class method
+            f"{objname}.{prefix}5{suffix}()", # static method
+        ]
+
+    def test_default_filter(self):
+        c = self.get_completer({self.objname: Object()})
+        self.add_filter(c, self.new_filter())
+
+        for p in self.prefixes:
+            with self.subTest(p):
+                matches = c.get_matches(f'{self.objname}.{p}')
+                expects = self.make_matches(self.objname, p)
+                self.assertListEqual(matches, expects)
+
+    def test_simple_filter(self):
+        for p in self.prefixes:
+            with self.subTest(p):
+                c = self.get_completer({self.objname: Object()})
+                self.add_filter(c, self.SkipIfContains(p))
+                matches = c.get_matches(f'{self.objname}.{p}')
+                self.assertListEqual(matches, [])
+
+    def test_chain_filters(self):
+        for r in range(len(self.prefixes)):
+            for ps in itertools.combinations(self.prefixes, r=r + 2):
+                c = self.get_completer({self.objname: Object()})
+                # add many filters
+                for p in ps:
+                    self.add_filter(c, self.SkipIfContains(p))
+
+                for p in ps:
+                    with self.subTest('exclude', filters=ps, prefix=p):
+                        matches = c.get_matches(f'{self.objname}.{p}')
+                        self.assertListEqual(matches, [])
+
+                # check that the remaining objects are completed
+                for p in set(self.prefixes).difference(ps):
+                    with self.subTest('include', filters=ps, prefix=p):
+                        matches = c.get_matches(f'{self.objname}.{p}')
+                        expects = self.make_matches(self.objname, p)
+                        self.assertListEqual(matches, expects)
+
 
 if __name__ == '__main__':
     unittest.main()
